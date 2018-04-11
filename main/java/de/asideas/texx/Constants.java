546b819e6deb509116a2e8e7f16ceb2b4d245d1e
BWagnerASIdeas <babette.wagner@asideas.de>
2018-04-11 15:39:11 +0200

    fresh start

--- main/java/de/asideas/texx/Constants.java
+++
@@ -1,6 +0,0 @@
-package de.asideas.texx;
-
-public class Constants {
-    public static final String LEBENSMITTEL = "Lebensmittel";
-    public static final String DROGERIEARTIKEL = "Drogerieartikel";
-}
--- main/java/de/asideas/texx/data/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package de.asideas.texx.data;
-
-import de.asideas.texx.model.Shop;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ShopRepository extends CrudRepository<Shop, Long>{
-}
